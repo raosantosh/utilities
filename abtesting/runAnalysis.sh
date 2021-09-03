8d4ddfad4630bbf9a6ded9e0c376a73ddad8c3f2
raosantosh <rao.santosh81@yahoo.com>
2021-09-03 10:05:51 -0400

    Delete abtesting directory

--- abtesting/runAnalysis.sh
+++
@@ -1,1 +0,0 @@
-python abAnalysisTemplateClicksSpend.py srao_kwm_15_26nov_all_v1 "Target,Peapod,Kohls,Staples,Toys R Us ATG,Macys,Kmart,Sears,Costco" "2017-11-15,2017-11-16,2017-11-17,2017-11-18,2017-11-19,2017-11-20,2017-11-21,2017-11-22,2017-11-23,2017-11-24,2017-11-25,2017-11-26"
